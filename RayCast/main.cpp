60751ed3ffef45d1081b3bb422a635c678becfaa
Dener Miranda <denner.miranda@gmail.com>
2016-10-17 16:41:38 -0300

    Adding main and project files

---
+++ RayCast/main.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <limits>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+using namespace std;
+
+struct RGB {
+    double r;
+    double g;
+    double b;
+};
+
+void saveBMP(const std::string filename, int w, int h, int dpi, RGBType* data){
+    FILE* file;
+    int totalSize = w * h;
+    int stride = 4 * totalSize;
+    int fileSize = 54 + stride;
+
+    double factor = 39.375;
+    int meter = static_cast<int>(factor);
+    int pixelsPerMeter = dpi * meter;
+
+    unsigned char bmpFileHeader[14] = {'B', 'M', 0,0,0,0, 0,0,0,0, static_cast<char>(54),0,0,0 };
+    unsigned char bmpInfoHeader[40];
+
+    std::memset(bmpInfoHeader, 0, sizeof(bmpInfoHeader));
+
+    bmpInfoHeader[0] = static_cast<char>(40);
+    bmpInfoHeader[12] = 1;
+    bmpInfoHeader[14] = 24;
+
+    //Header: Signature, Filesize, Reserved1, Reserved2, File Offset to Pixel Array
+    bmpFileHeader[2] = (unsigned char)(fileSize);
+    bmpFileHeader[3] = (unsigned char)(fileSize >> 8);
+    bmpFileHeader[4] = (unsigned char)(fileSize >> 16);
+    bmpFileHeader[5] = (unsigned char)(fileSize >> 24);
+
+    //Width of the bitmap
+    bmpInfoHeader[4] = (unsigned char)(w);
+    bmpInfoHeader[5] = (unsigned char)(w>>8);
+    bmpInfoHeader[6] = (unsigned char)(w>>16);
+    bmpInfoHeader[7] = (unsigned char)(w>>24);
+
+    //Height of the bitmap
+    bmpInfoHeader[8] = (unsigned char)(h);
+    bmpInfoHeader[9] = (unsigned char)(h>>8);
+    bmpInfoHeader[10] = (unsigned char)(h>>16);
+    bmpInfoHeader[11] = (unsigned char)(h>>24);
+
+    //Pixels per meter Density
+    bmpInfoHeader[21] = (unsigned char)(pixelsPerMeter);
+    bmpInfoHeader[22] = (unsigned char)(pixelsPerMeter>>8);
+    bmpInfoHeader[23] = (unsigned char)(pixelsPerMeter>>16);
+    bmpInfoHeader[24] = (unsigned char)(pixelsPerMeter>>24);
+
+    bmpInfoHeader[25] = (unsigned char)(pixelsPerMeter);
+    bmpInfoHeader[26] = (unsigned char)(pixelsPerMeter>>8);
+    bmpInfoHeader[27] = (unsigned char)(pixelsPerMeter>>16);
+    bmpInfoHeader[28] = (unsigned char)(pixelsPerMeter>>24);
+
+    bmpInfoHeader[29] = (unsigned char)(pixelsPerMeter);
+    bmpInfoHeader[30] = (unsigned char)(pixelsPerMeter>>8);
+    bmpInfoHeader[31] = (unsigned char)(pixelsPerMeter>>16);
+    bmpInfoHeader[32] = (unsigned char)(pixelsPerMeter>>24);
+
+    file = std::fopen(filename.c_str(), "wb"); //wb := 'W'rite 'B'inary
+
+    std::fwrite(bmpFileHeader, 1, 14, file);
+    std::fwrite(bmpInfoHeader, 1, 40, file);
+
+    for (int i =0; i<totalSize; i++){
+        RGBType rgb = data[i];
+
+        double red = (data[i].r)*255;
+        double green = (data[i].g)*255;
+        double blue = (data[i].b)*255;
+
+        unsigned char color[3] = {
+            static_cast<unsigned char>((int) std::floor(blue)),
+            static_cast<unsigned char>((int) std::floor(green)),
+            static_cast<unsigned char>((int) std::floor(red))
+        };
+
+        std::fwrite(color, 1, 3, file);
+    }
+
+    std::fclose(file);
+}
+
+int main(int argc, char *argv[])
+{
+    cout << "Rendering" << endl;
+
+    int width = 640;
+    int height = 480;
+
+    for (int x = 0; x< width; c++){
+
+        for (int y = 0; y< height; c++){
+            //return colour
+        }
+    }
+
+    return 0;
+}
